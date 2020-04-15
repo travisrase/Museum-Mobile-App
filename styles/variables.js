b1f3f1b0682e2b1440e54a492b314db428bed4b8
Max Tanous <maxtanous@Maxs-MacBook-Pro-2.local>
2020-04-15 13:13:53 -0600

    fixed Spelling

---
+++ styles/variables.js
@@ -0,0 +1,26 @@
+let GlobalVariables;
+
+export default GlobalVariables = {
+    container: {
+        marginTop: 150,
+        backgroundColor: 'blue',
+        flexWrap: 'wrap'
+    },
+    primaryButton: {
+        flex: 1,
+        height: 70,
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 20,
+        marginRight: 20
+    },
+    commonColors: {
+        color: '#29acdc',
+        backgroundGray: '#ececec',
+        grayColor: 'gray',
+        lightGrayBackground: '#e7e7e7'
+    },
+};
+
+
